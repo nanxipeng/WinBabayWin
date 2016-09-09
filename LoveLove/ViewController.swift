cce2d8ba2e2eb539fec0b4487d9b42ac5a31136c
彭楠稀 <pengnanxi@houkusunokimares-macbook-air.local>
2016-09-09 14:12:10 -0400

    Initial Commit

---
+++ LoveLove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveLove
+//
+//  Created by 彭楠稀 on 9/9/16.
+//  Copyright © 2016 Nanxi Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
